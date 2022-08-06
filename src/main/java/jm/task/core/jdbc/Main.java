00f3860b4f1fb4bf4f35ac64a0d0dd1ee1d768c3
Artem311k <101522028+Artem311k@users.noreply.github.com>
2022-08-07 01:47:17 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,16 +7,16 @@ public class Main {
         // реализуйте алгоритм здесь
         UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
-        userDaoJDBC.createUsersTable();
+        userService.createUsersTable();
 
-        userDaoJDBC.saveUser("User name one", "User LatName one", (byte) 10);
-        userDaoJDBC.saveUser("User name two", "User LatName two", (byte) 15);
-        userDaoJDBC.saveUser("User name three", "User LatName three", (byte) 20);
-        userDaoJDBC.saveUser("User name four", "User LatName four", (byte) 25);
+        userService.saveUser("User name one", "User LatName one", (byte) 10);
+        userService.saveUser("User name two", "User LatName two", (byte) 15);
+        userService.saveUser("User name three", "User LatName three", (byte) 20);
+        userService.saveUser("User name four", "User LatName four", (byte) 25);
 
-        userDaoJDBC.getAllUsers().forEach(System.out::println);
-        userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.dropUsersTable();
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
     }
 }
